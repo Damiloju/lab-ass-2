3ad34a22255510b6da74c16bb4f7798f6d01ce7f
Oluwadamiloju Yusuf <yusufdamiloju@gmail.com>
2022-03-05 21:36:59 +0200

    define main functions for timer and gpio module

--- timer_handler.c
+++ timer_handler.c
@@ -2,7 +2,7 @@
  * @file timer_handler.c
  *
  * @brief Init TIMER0 to control buzzer with PWM signal.
- * 
+ *
  * @author Johannes Ehala, ProLab.
  * @license MIT
  *
@@ -15,12 +15,57 @@
 #include "timer_handler.h"
 
 /**
- * @brief Init TIMER0 to regulate PWM dutycycle. 
+ * @brief Gpio_init function.
+ * Initialize GPIO and set up pins for leds
  */
-void timer0_init(void)
+void led_gpio_init()
 {
-	// Enable clocks.
+    // Enable clock for GPIO module
+    CMU_ClockEnable(cmuClock_GPIO, true);
+
+    // Set up LED pins
+    GPIO_PinModeSet(LED_RED_PORT, LED_RED_PIN, gpioModePushPull, 0);
+    GPIO_PinModeSet(LED_GREEN_PORT, LED_GREEN_PIN, gpioModePushPull, 0);
+    GPIO_PinModeSet(LED_BLUE_PORT, LED_BLUE_PIN, gpioModePushPull, 0);
+}
+
+/**
+ * @brief Init TIMER0 to regulate PWM dutycycle.
+ */
+uint32_t timer0_init(void)
+{
+    // Enable clocks.
     CMU_ClockEnable(cmuClock_TIMER0, true);
 
     // TODO Init
+    // Init CC for Output Compare on GPIO pins.
+    TIMER_InitCC_TypeDef ccInit = TIMER_INITCC_DEFAULT;
+    ccInit.mode = timerCCModeCompare;
+    ccInit.cmoa = timerOutputActionToggle;
+
+    // Initilize a CC channels for toggling leds.
+    TIMER_InitCC(TIMER0, 0, &ccInit);
+
+    // Enable GPIO toggling by TIMER and set location of led pins to be toggled.
+    TIMER0->ROUTEPEN = TIMER_ROUTEPEN_CC0PEN;
+    TIMER0->ROUTELOC0 = LED_RED_LOCATION | LED_GREEN_LOCATION | LED_BLUE_LOCATION;
+
+    // Set same TIMER0 top value for all CC channels.
+    TIMER_TopSet(TIMER0, TIMER0_TOP_VAL);
+
+    // Timer general initialization and config.
+    TIMER_Init_TypeDef timerInit = TIMER_INIT_DEFAULT;
+    timerInit.prescale = TIMER0_CLOCK_DIV;
+    timerInit.enable = true; // Start timer after init.
+
+    TIMER_Init(TIMER0, &timerInit);
+
+    // return calculated frequncy
+    return (uint32_t)(CMU_ClockFreqGet(cmuClock_TIMER0) / (uint32_t)(0x1 << TIMER0_CLOCK_DIV));
+}
+
+// util function to set top value/duty cycle for timer0
+void timer0_set_top_val(uint32_t tv)
+{
+    TIMER_TopBufSet(TIMER0, tv);
 }
--- timer_handler.h
+++ timer_handler.h
@@ -10,7 +10,24 @@
 #ifndef TIMER_HANDLER_H_
 #define TIMER_HANDLER_H_
 
+#include "em_gpio.h" /// import GPIO
+
+#define LED_RED_PORT gpioPortB   //!< Port for red led.
+#define LED_GREEN_PORT gpioPortB // !< Port for green led.
+#define LED_BLUE_PORT gpioPortA  // !< Port for blue led.
+
+#define LED_RED_PIN 12                     // !< Pin for red led.
+#define LED_GREEN_PIN 11                   // !< Pin for green led.
+#define LED_BLUE_PIN 5                     // !< Pin for blue led.
+#define TIMER0_CLOCK_DIV timerPrescale1024 // Timer0 clock divider
+#define TIMER0_TOP_VAL 100                 // Timer0 top value
+#define LED_RED_LOCATION 0                 // !< Location for red led.
+#define LED_GREEN_LOCATION 0               // !< Location for green led.
+#define LED_BLUE_LOCATION 0                //      !< Location for blue led.
+
 // Public functions
-void timer0_init();
+void led_gpio_init();                 // Initialize GPIO and set up pins for leds
+uint32_t timer0_init(void);           // Init TIMER0 to regulate PWM dutycycle
+void timer0_set_top_val(uint32_t tv); // Set TIMER0 top value
 
 #endif // TIMER_HANDLER_H_ */
